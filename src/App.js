3610d74480b2caee3cf375f12ca60551739389fa
vishal2796 <vishal.pb.27@gmail.com>
2023-05-20 18:41:26 +0530

    added expense item props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
-  const elements = [
+  const expenses = [
     { title: "Food", date: new Date(2023, 2, 4), price: "$50" },
     { title: "Petrol", date: new Date(2023, 2, 16), price: "$100" },
     { title: "Movie", date: new Date(2023, 2, 22), price: "$200" },
@@ -9,8 +9,16 @@ function App() {
   return (
     <div>
       <h2>Expense Tracker!</h2>
-      <div></div>
-      <ExpenseItem></ExpenseItem>
+      <div>
+        {expenses.map((expense) => (
+          <ExpenseItem
+            key={expense.date}
+            title={expense.title}
+            date={expense.date}
+            price={expense.price}
+          ></ExpenseItem>
+        ))}
+      </div>
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,27 +1,20 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
+function ExpenseItem(props) {
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear();
   return (
     <>
       <div className="expense-item">
-        <div>March 28th 2023</div>
-        <div className="expense-item__description">
-          <h2>Food</h2>
-          <div className="expense-item__price">$20</div>
+        <div>
+          <div>{month}</div>
+          <div>{year}</div>
+          <div>{day}</div>
         </div>
-      </div>
-      <div className="expense-item">
-        <div>March 28th 2023</div>
         <div className="expense-item__description">
-          <h2>Petrol</h2>
-          <div className="expense-item__price">$100</div>
-        </div>
-      </div>
-      <div className="expense-item">
-        <div>March 28th 2023</div>
-        <div className="expense-item__description">
-          <h2>Movie</h2>
-          <div className="expense-item__price">$154</div>
+          <h2>{props.title}</h2>
+          <div className="expense-item__price">{props.price}</div>
         </div>
       </div>
     </>
