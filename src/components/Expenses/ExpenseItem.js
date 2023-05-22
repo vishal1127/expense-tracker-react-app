1fa6b2b3e061b4fdb98904ceec8e74f8e56f0037
vishal2796 <vishal.pb.27@gmail.com>
2023-05-22 21:29:12 +0530

    added delete expense button

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -2,12 +2,17 @@ import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
+
 const ExpenseItem = (props) => {
+  const deleteHandler = () => {
+    console.log("clicked");
+  };
   return (
     <>
       <Card className="expense-item">
         <ExpenseDate date={props.date} />
         <ExpenseDetails title={props.title} price={props.price} />
+        <button onClick={deleteHandler}>Delete expense</button>
       </Card>
     </>
   );
