1edb7717e6d467f092e8dc8cee90864995cac565
vishal2796 <vishal.pb.27@gmail.com>
2023-05-23 20:54:58 +0530

    added creating new expense feature

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,25 +1,14 @@
-import { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-  const [amount, setAmount] = useState(props.amount);
-  const updateAmount = () => {
-    setAmount("$100");
-  };
-  const [title, setTitle] = useState(props.title);
-  const deleteHandler = () => {
-    setTitle("New");
-  };
   return (
     <>
       <Card className="expense-item">
         <ExpenseDate date={props.date} />
-        <ExpenseDetails title={title} amount={amount} />
-        <button onClick={deleteHandler}>Delete expense</button>
-        <button onClick={updateAmount}>Change amount</button>
+        <ExpenseDetails title={props.title} amount={props.amount} />
       </Card>
     </>
   );
