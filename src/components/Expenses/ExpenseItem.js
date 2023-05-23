476d61dc62c682fd5564b31b47adaa122d4d0408
vishal2796 <vishal.pb.27@gmail.com>
2023-05-23 15:53:34 +0530

    implemented useState hook

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,25 @@
+import { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  const [price, setPrice] = useState(props.price);
+  const updatePrice = () => {
+    setPrice("$100");
+  };
+  const [title, setTitle] = useState(props.title);
   const deleteHandler = () => {
-    console.log("clicked");
+    setTitle("New");
   };
   return (
     <>
       <Card className="expense-item">
         <ExpenseDate date={props.date} />
-        <ExpenseDetails title={props.title} price={props.price} />
+        <ExpenseDetails title={title} price={price} />
         <button onClick={deleteHandler}>Delete expense</button>
+        <button onClick={updatePrice}>Change price</button>
       </Card>
     </>
   );
